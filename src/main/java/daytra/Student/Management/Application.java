02c0fb7ebd78a534363f162555e209d3fdab8acd
gobird <monaka@example.com>
2025-03-01 22:09:05 +0900

    first commit

---
+++ src/main/java/daytra/Student/Management/Application.java
@@ -0,0 +1,23 @@
+package daytra.Student.Management;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/えい")
+	public String hello() {
+		return "ちょや";
+	}
+
+
+}
